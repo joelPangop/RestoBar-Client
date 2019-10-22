4bb537e94267f33bc63f00fbacd1546b75e690e4
joelPangop <joelpangop@gmail.com>
2019-10-21 21:21:27 -0700

    commit 2

---
+++ src/app/models/produit-achat.spec.ts
@@ -0,0 +1,7 @@
+import { ProduitAchat } from './produit-achat';
+
+describe('ProduitAchat', () => {
+  it('should create an instance', () => {
+    expect(new ProduitAchat()).toBeTruthy();
+  });
+});
